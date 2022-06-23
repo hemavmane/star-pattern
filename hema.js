7a093e2c4e50fd14843d833ac888667e64bc0db1
hemamane <hemamane5376@gmail.com>
2022-06-23 17:42:27 +0530

    star

---
+++ hema.js
@@ -0,0 +1,37 @@
+//inverted triangle
+// let n = 10;
+// for(let i=1; i<=n; i++){
+// for(let j =i; j<=n; j++){
+//     document.write("*")
+// }document.write("</br>")
+// } 
+
+
+//vertical  triangle
+
+// let n = 10;
+// for(let i=1; i<=n; i++){
+// for(let j =1; j<=i; j++){
+//     document.write("*")
+// }document.write("</br>")
+// } 
+
+// paramid triangle
+// let n = 10;
+// for(let i=1; i<=n; i++){
+//     for(let j =i; j<=n; j++){
+//         document.write("<")
+//     }
+// for(let j =1; j<i; j++){
+//     document.write("*")
+// }
+// for(let j =1; j<=i; j++){
+//     document.write("*")
+// }
+
+// document.write("</br>")
+// } 
+
+
+
+
